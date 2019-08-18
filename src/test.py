1c6386d3ce251ecaaf6adfa564f1b695a68150c6
Hugo VERJUS <hugo.verjus@epita.fr>
2019-08-18 13:25:44 +0100

    up: changed number

--- src/test.py
+++ src/test.py
@@ -11,12 +11,12 @@ from neopixel import *
 import argparse
 
 # LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
+LED_COUNT      = 27      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 2     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -79,24 +79,32 @@ def theaterChaseRainbow(strip, wait_ms=50):
             for i in range(0, strip.numPixels(), 3):
                 strip.setPixelColor(i+q, 0)
 
+def fullwhite(strip, wait_ms=50):
+    for i in range(0, strip.numPixels(), 3):
+        strip.setPixelColor(i+q, wheel((i+j) % 255))
+
+
+
+
 def build_map():
     dico = {}
     dico[0]
     return dico
+
 
 def main_func():
     print ('Color wipe animations.')
     colorWipe(strip, Color(255, 0, 0))  # Red wipe
     colorWipe(strip, Color(0, 255, 0))  # Blue wipe
     colorWipe(strip, Color(0, 0, 255))  # Green wipe
-    print ('Theater chase animations.')
-    theaterChase(strip, Color(127, 127, 127))  # White theater chase
-    theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-    theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-    print ('Rainbow animations.')
-    rainbow(strip)
-    rainbowCycle(strip)
-    theaterChaseRainbow(strip)
+    #print ('Theater chase animations.')
+    #theaterChase(strip, Color(127, 127, 127))  # White theater chase
+    #theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+    #theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+    #print ('Rainbow animations.')
+    #rainbow(strip)
+    #rainbowCycle(strip)
+    #theaterChaseRainbow(strip)
 
 
 # Main program logic follows:
